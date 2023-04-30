a02e63f6c00f3d31bad5969fcf68b894b7670b37
manisaiprasad <manisaiprasadam@gmail.com>
2023-04-30 00:59:12 -0400

    items upload

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,12 +1,15 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
+import { getStorage } from 'firebase-admin/storage';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 const app = initializeApp({
   credential: cert(serviceAccount),
+  storageBucket: 'gs://lostfoundstevens.appspot.com',
 });
 
-const auth = getAuth(app);
-export default auth;
+export const storage = getStorage(app);
+export const auth = getAuth(app);
--- server/routes/items.js
+++ server/routes/items.js
@@ -1,1 +1,9 @@
 import express from 'express';
+import { VerifyToken } from '../middleware/VerifyToken.js';
+import { report } from '../controllers/items.js';
+const router = express.Router();
+import upload from '../utils/uploadImage.js';
+
+router.post('/report', VerifyToken, upload.single('image'), report);
+
+export default router;
