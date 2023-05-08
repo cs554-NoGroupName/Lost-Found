6772d616a35e72213f749a93b3a4efbc49164aff
manisaiprasad <manisaiprasadam@gmail.com>
2023-05-07 21:41:08 -0400

    redis config update

--- server/middleware/items.js
+++ server/middleware/items.js
@@ -1,13 +1,23 @@
-import redis from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
+import redis from 'redis';
+
+// const client = redis.createClient({
+//   password: process.env.REDIS_PASSWORD,
+//   socket: {
+//     port: process.env.REDIS_PORT || 6379,
+//     host: process.env.REDIS_HOST || 'redis',
+//   },
+// });
+
 const client = redis.createClient({
-  socket: {
-    port: 6379,
-    host: 'redis',
-  },
+  password: process.env.REDIS_PASSWORD,
+  url: `rediss://${process.env.REDIS_HOST}:6380`,
 });
-client.connect().then(() => {});
+
+client.connect().then(() => {
+  console.log('Redis connected');
+});
 
 export const getAllItems = async (req, res, next) => {
   let exists = await client.exists('getItem');
@@ -21,13 +31,17 @@ export const getAllItems = async (req, res, next) => {
 
 export const getItemsById = async (req, res, next) => {
   console.log('In get items by Id');
-  let exists = await client.exists(`Item_${req.params.id}`);
-  if (exists) {
-    console.log('Sending data from redis');
-    let getItem = await client.get(`Item_${req.params.id}`);
-    // return res.status(200).json({ data: JSON.parse(getItem) });
-    res.status(200).json({ data: JSON.parse(getItem) });
-  } else next();
+  try {
+    let exists = await client.exists(`Item_${req.params.id}`);
+    if (exists) {
+      console.log('Sending data from redis');
+      let getItem = await client.get(`Item_${req.params.id}`);
+      // return res.status(200).json({ data: JSON.parse(getItem) });
+      res.status(200).json({ data: JSON.parse(getItem) });
+    } else next();
+  } catch (e) {
+    next();
+  }
 };
 
 // export const updateReportedItem = async (req, res, next) => {
